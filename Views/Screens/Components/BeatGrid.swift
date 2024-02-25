137c76b40819f67b3ec8af09bc52bf847f744cc3
Jared Drueco <drueco03@gmail.com>
2024-02-25 01:08:53 -0700

    Handled beat UI out of screen bug

--- Views/Screens/Components/BeatGrid.swift
+++ Views/Screens/Components/BeatGrid.swift
@@ -18,11 +18,10 @@ struct BeatGrid: View {
             let padding: CGFloat = (geometry.size.width / CGFloat(trackViewModel.beatsPerBar * trackViewModel.numberOfBars)) / 2
             let timeToPositionConversion = geometry.size.width / trackViewModel.trackModel.totalDuration
             ZStack {
-                // Drawing dividers and spacers for each beat row
                 VStack(spacing: 0) {
                     ForEach(0..<numberOfBeats, id: \.self) { padID in
                         Divider().background(Color.black)
-                        Spacer() // Creates a spacer that will be used to separate each beat row
+                        Spacer()
                     }
                 }
                 
@@ -33,14 +32,15 @@ struct BeatGrid: View {
                     
                     ForEach(beatsForRow, id: \.startTime) { beat in
                         // Calculate x position based on beat
-                        let beatXPosition = padding + (beat.startTime * CGFloat(timeToPositionConversion)) + (CGFloat(beat.barNumber) * (barWidth))
+                        let adjustedStartTime = min(beat.startTime, 3.5) // Ensure not to go over UI on right side. 
+                        let beatXPosition = padding + (adjustedStartTime * CGFloat(timeToPositionConversion)) + (CGFloat(beat.barNumber) * (barWidth))
                         
                         // Calculate y position based on pad ID
                         let rowHeight = geometry.size.height / CGFloat(numberOfBeats)
                         let beatYPosition = rowHeight * CGFloat(padID) + rowHeight / 2
                         
                         Rectangle()
-                            .fill(Color.blue)
+                            .fill(Color.black)
                             .frame(width: 10, height: rowHeight) // Width is placeholder for now as there is no hold duration logic yet.
                             .position(x: beatXPosition, y: beatYPosition)
                     }
